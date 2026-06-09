91c9a8e9c5e527516431bc258f7879d62ee9475c
Wincc88 <tngmagnum@gmail.com>
2026-06-09 19:24:57 +0800

    add index and script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather App
+It takes the Api Object
+Object key/values is taken 
+It reveals the current weather information based on country and city chosen
+
+##
+Form is used to get user's inputed country and city
+
+### 
---
+++ makescipt.js
@@ -0,0 +1,150 @@
+
+
+/*
+async function getweather () {
+    
+    try {
+        
+        
+        // const response = await fetch(`https:// weather app`);
+                
+        const weatherData = await response.json();
+        
+        console.log(weatherData);
+                
+
+
+       
+            
+        toCurrency.addEventListener('change', function() {
+            const selectedCurrency = toCurrency.options[toCurrency.selectedIndex].value;
+            if (allRates[selectedCurrency]) {
+                console.log(`USD to ${selectedCurrency}: ${allRates[selectedCurrency]}`);
+                middleResultDisplay.textContent = allRates[selectedCurrency];
+
+            } else {
+                console.log(`No rate for ${selectedCurrency} in response`);
+                middleResultDisplay.textContent = `No rate for ${selectedCurrency} available  ..yet!`;
+            }
+            console.log(`Selected currency on change: ${selectedCurrency}`);
+        });
+                        
+                // Set EUR rate on load -- it awaits and takes the result from fetch to select EUR  
+                        // another api use eur as base, so need to switch 
+                    
+                // i dont need special windows onload or DOMContentLoaded event listeners..
+                //.. because the script is at the end of the body, so it will run after the DOM is loaded.
+        middleResultDisplay.textContent = weatherData.rates.EUR;    //`${weatherData.conversion_rates.EUR}`;
+        console.log(`selected on load EUR = ${weatherData.rates.EUR}`)          //(`Selected currency on load: EUR = ${weatherData.conversion_rates.EUR}`); // Log the default selected currency on load
+        // ratesDisplays.textContent = weatherData.rates.EUR
+
+
+    } catch (error) {
+        console.error('Error fetching rates:', error);
+        middleResultDisplay.textContent = 'Error fetching rates';
+    }
+
+}  
+
+getweather();  
+
+*/
+
+
+ const fetchedApiData = [
+
+    { date: '2026-06-05', day: 'Friday', weather: 'rainfall', status: 'heavy rain', degrees:'21°C' },
+    { date: '2026-06-06', day: 'Saturday', weather: 'sunshine', status: 'sunny', degrees:'30°C' },
+    { date: '2026-06-07', day: 'Sunday', weather: 'snow', status: 'snowing', degrees: '-10°C' },
+    { date: '2026-06-08', day: 'Monday', weather: 'wind', status: 'thunder',degrees: '0°C' },
+    { date: '2026-06-09', day: 'Tuesday', weather: 'clody', status: 'lightning', degrees: '15°C' }
+
+] 
+
+ const showTodayInfo = document.querySelector('.current_dayweather');
+
+
+ function getDateRangeApiData(apiData) {
+                
+        // Convert API array to a Map for O(1) fast lookups
+        const apiMap = new Map(apiData.map(item => [item.date, item]));
+                //console.log(`here is apiepepepeep : ${apiMap}`);
+        const dates = [];
+        const days = [];
+            // let matchingApiData; 
+
+
+
+        for (let i = 0; i < fetchedApiData.length; i++) {
+            
+            //len start count from 1 not 0 so i < and not <=  else it will add undefined as last arraynumber;
+
+            const current_date = new Date();
+
+            const yesterdaysDate = new Date(current_date);
+            yesterdaysDate.setDate(current_date.getDate() - 1);
+            
+            current_date.setDate(current_date.getDate() - i);
+               // console.log('dateCheckhere: ', current_date);
+
+                
+            
+            const dateString = current_date.toISOString().split('T')[0];
+                // console.log(`dateStringHere: ${dateString}`);
+            
+            const yesterdaysDateString = yesterdaysDate.toISOString().split('T')[0];
+                // console.log(`yesterdaysDateStringHere: ${yesterdaysDateString}`);
+            
+            const dayName = current_date.toLocaleDateString('en-US', { weekday: 'long' });
+                // console.log(`dayNameHere: ${dayName}`);
+
+            function nameTodayAndYesterday() {
+
+                if (dateString === new Date().toISOString().split('T')[0]) {
+                    // using dayName === ; gives same Today like Monday this week and Monday last week
+                    return "Today";
+                }
+                else if (dateString === yesterdaysDateString) {
+                        console.log("Yesterday");
+                    return "Yesterday";
+                }
+                
+              
+
+
+            //console.log(dayName);   
+                return dayName;
+
+            };
+                
+            nameTodayAndYesterday();
+
+         
+        
+        const matchingApiData = apiMap.get(dateString);
+
+        console.log(matchingApiData.weather);
+        console.log(matchingApiData.status);
+        console.log(matchingApiData.degrees);
+        console.log(matchingApiData.date);
+        console.log(matchingApiData.day)
+
+        dates.push(matchingApiData);
+        console.log(matchingApiData);
+
+        }
+
+             //console.log(dates);
+        return dates;
+
+
+
+ }
+ console.log(getDateRangeApiData(fetchedApiData)); 
+
+
+ 
+
+
+
+
