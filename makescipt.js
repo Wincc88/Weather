70dabdedcd9b32fff2a7fcbb8a0c015dca132824
Wincc88 <tngmagnum@gmail.com>
2026-06-15 19:09:01 +0800

    add fahrenheit change

--- makescipt.js
+++ makescipt.js
@@ -51,6 +51,9 @@ getweather();
 */
 
 
+
+
+
  const fetchedApiData = [
 
     { date: '2026-06-05', day: 'Friday', weather: 'rainfall', status: 'heavy rain', degrees:'21°C|°F' },
@@ -58,11 +61,19 @@ getweather();
     { date: '2026-06-07', day: 'Sunday', weather: 'snow', status: 'snowing', degrees: '-10°C|°F' },
     { date: '2026-06-08', day: 'Monday', weather: 'wind', status: 'thunder',degrees: '0°C|°F' },
     { date: '2026-06-09', day: 'Tuesday', weather: 'cloudy', status: 'lightning', degrees: '15°C|°F' },
-    { date: '2026-06-10', day: 'Wednesday', weather: 'sushine', status: 'bright', degrees: '24°C|°F' }
-
+    { date: '2026-06-10', day: 'Wednesday', weather: 'sushine', status: 'bright', degrees: '24°C|°F' },
+    { date: '2026-06-11', day: 'Thursday', weather: 'sushine', status: 'bright', degrees: '24°C|°F' },
+    { date: '2026-06-12', day: 'Friday', weather: 'rainfall', status: 'heavy rain', degrees:'21°C|°F' },
+    { date: '2026-06-13', day: 'Saturday', weather: 'sunshine', status: 'sunny', degrees:'30°C|°F' },
+    { date: '2026-06-14', day: 'Sunday', weather: 'snow', status: 'snowing', degrees: '-10°C|°F' },
+    { date: '2026-06-15', day: 'Monday', weather: 'wind', status: 'thunder',degrees: '32°C|°F' }
 ] 
 
- const showTodayInfo = document.querySelector('.current_dayweather');
+ const showTodayInfo = document.querySelector('.grid-container'); 
+ const temperature = document.querySelector('.temperature');
+ const status = document.querySelector('.status');
+ const timeAndDay = document.querySelector('.time_day');
+ const celcfahToggle = document.querySelector('.celfahdisplay');
 
 
  function getDateRangeApiData(apiData) {
@@ -103,17 +114,18 @@ getweather();
 
                 if (dateString === new Date().toISOString().split('T')[0]) {
                     // using dayName === ; gives same Today like Monday this week and Monday last week
+                    console.log("Today");
                     return "Today";
                 }
                 else if (dateString === yesterdaysDateString) {
-                        console.log("Yesterday");
+                    console.log("Yesterday");
                     return "Yesterday";
                 }
                 
               
 
 
-            //console.log(dayName);   
+             console.log(dayName);   
                 return dayName;
 
             };
@@ -123,15 +135,36 @@ getweather();
          
         
         const matchingApiData = apiMap.get(dateString);
-
+        /*
         console.log(matchingApiData.weather);
         console.log(matchingApiData.status);
         console.log(matchingApiData.degrees);
         console.log(matchingApiData.date);
         console.log(matchingApiData.day)
 
+        const latestWeather = matchingApiData.weather
+                
+        const previousDate = new Date(current_date);
+        previousDate.setDate(current_date.getDate() - 1);
+        const previousDateString = previousDate.toISOString().split('T')[0];
+        const previousApiData = apiMap.get(previousDateString);
+        const previousWeather = previousApiData ? previousApiData.weather : null;
+        console.log(`latestWeather: ${latestWeather}, previousWeather: ${previousWeather} (from ${previousDateString})`);
+        */
+        
+
         dates.push(matchingApiData);
         console.log(matchingApiData);
+        
+           //  using this for latest entry only
+         const myyy =  Object.values(fetchedApiData[fetchedApiData.length - 1]);
+       // temperature.textContent = myyy[myyy.length -1];
+        
+        let myyy2;
+        status.textContent = myyy[myyy.length -2];
+         
+        timeAndDay.textContent = `Today: ${myyy[0]}`;
+        
 
         }
 
@@ -144,20 +177,88 @@ getweather();
  console.log(getDateRangeApiData(fetchedApiData)); 
 
 
+
  function celciusFromFahr (fahrenheit) {
 
         /* (number)  -> number
            celciusFromFahr (32) returns 0 celc  ; celciusFromFah (212) returns 100 celc
 
-           change formula if celcius to Fahr (celcius * 9/5) + 32;
+           change formula if celcius to Fahr (fahrenheit - 32) * (5/9);
        */
-    const answer = (fahrenheit - 32) * (5/9);
+    const answer = (fahrenheit * 9/5) + 32;         
       //console.log(answer);
     return answer;
 
  }
  console.log(celciusFromFahr(32));
+ console.log(celciusFromFahr(20));
 
+
+
+
+    const para = document.createElement("p");
+    para.classList.add("Temppara");
+
+
+    const latestObj = Object.values(fetchedApiData[fetchedApiData.length - 1]);  // latest object 
+         // console.log(Object.values(fetchedApiData));
+          //  para.textContent = myyy[myyy.length-1];
+          //  temperature.appendChild(para);
+          
+    // console.log(para.textContent.includes("°C"));
+      const degreesStr = latestObj[latestObj.length - 1]; // e.g. '24°C|°F'
+         //console.log(degreesStr);
+      para.textContent = degreesStr;      // intialized
+      temperature.appendChild(para);
+
+      let celcshowing = true;      // first in celcius from api, initialize 
+
+
+
+
+temperature.addEventListener("click", () => {
+  //  if (degreesStr.includes("°C")) {    // includes is redundant as it is the °C|°F string itself.
+    if (degreesStr) {
+    
+        //get and store degreeStr from index 0 to but not including °C
+        const idxCtoend = degreesStr.indexOf('°C');   // index number 
+        console.log(idxCtoend);
+
+        const storeStr = degreesStr.slice(0, idxCtoend).trim();    // get only number part
+        console.log (storeStr);
+
+        const strToend = degreesStr.slice(-2,).trim();     // use only °F
+        console.log(strToend);
+        
+        // turn storeStr into number 
+        const numSide = parseFloat(storeStr);
+
+
+    
+        // when click c or f change display using celciusFromFar 
+         if (!isNaN(numSide)) {
+            const converted = celciusFromFahr(numSide);
+            const showInpara = `${converted}${strToend}` ;
+
+
+                if (celcshowing) {
+                    para.textContent = showInpara;
+                        //  console.log(para.textContent);
+                    temperature.appendChild(para);
+                } 
+                else {
+                    para.textContent = degreesStr;
+                        //  console.log(para.textContent);
+                    temperature.appendChild(para);
+                }
+                celcshowing = !celcshowing;       
+        
+    }
+       
+    }
+});
+
+    
 
 
 
